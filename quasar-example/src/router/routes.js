de1e7d72cfb109e3d4a35ac5ac7295f527170c06
abb21timsjo <tim.sjostrand@abbgymnasiet.se>
2023-02-13 15:24:48 +0100

    skapade recept sida

--- quasar-example/src/router/routes.js
+++ quasar-example/src/router/routes.js
@@ -23,6 +23,14 @@ const routes = [
       { path: '', component: () => import('pages/signupPage.vue')}
     ]
   },
+
+  {
+    path: '/recept',
+    component: () => import('layouts/MainLayout.vue'),
+    children: [
+      { path: '', component: () => import('pages/receptPage.vue')}
+    ]
+  },
   // Always leave this as last one,
   // but you can also remove it
   {
